93aa15af83a1d2de05eb54711112484a6f6fb3c5
Subham-Patro <dummy@gmail.com>
2026-03-28 05:41:07 +0000

    business/customer controller working

--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/BusinessController.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/BusinessController.java
@@ -11,10 +11,58 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@RestController
+@RequestMapping
 public class BusinessController {
     
-//Implement required code here!!!
+    @Autowired
+    private CargoService cargoService;
+
+    @Autowired
+    private DriverService driverService;
+
+    @PostMapping("/api/business/cargo")
+    public ResponseEntity<Cargo> addCargo( @RequestBody Cargo cargo ){
+        return new  ResponseEntity<Cargo>(cargoService.addCargo(cargo),HttpStatus.OK);
+    }
+
+    @GetMapping("/api/business/drivers")
+    public ResponseEntity<List<Driver>> getAllDrivers(){
+        return new ResponseEntity<List<Driver>>(driverService.getAllDrivers(),HttpStatus.OK);
+    }
+
+    @GetMapping("/api/business/cargo")
+    public ResponseEntity<List<Cargo>> getAllCargo(){
+        return new ResponseEntity<>(cargoService.viewAllCargo(),HttpStatus.OK);
+    }
+
+    @PostMapping("/api/business/assign-cargo")
+    public ResponseEntity<Map<String,String>> assignCargo(@RequestParam Long cargoId ,@RequestParam Long driverId){
+        Map<String,String> response = new HashMap<>();
+
+        try {
+            boolean assigned = cargoService.assignCargoToDriver(cargoId, driverId);
+
+            if (assigned) {
+                response.put("message", "Cargo assigned successfully");
+                return ResponseEntity.ok(response);
+            }else{
+                 response.put("message", "Failed to assign cargo");
+
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+            }
+        } catch (Exception e) {
+              response.put("message", "Failed to assign cargo");
+
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+      
+    }
+
+    
     
 }
--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/CustomerController.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/CustomerController.java
@@ -10,15 +10,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edutech.logisticsmanagementandtrackingsystem.dto.CargoStatusResponse;
+import com.edutech.logisticsmanagementandtrackingsystem.entity.Cargo;
+import com.edutech.logisticsmanagementandtrackingsystem.service.CargoService;
 import com.edutech.logisticsmanagementandtrackingsystem.service.CustomerService;
 
-
+@RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
 
-    
+    @Autowired
+    private CustomerService customerService;
+
 
         // get cargo status and return it with status code 200
-
+        // public ResponseEntity<CargoStatusResponse> viewCargoStatus(@RequestParam Long cargoId){
+        //     CargoStatusResponse cargoStatusResponse = customerService.
+        // }
         // if cargo status is not found, return 404 status code
     }
 
