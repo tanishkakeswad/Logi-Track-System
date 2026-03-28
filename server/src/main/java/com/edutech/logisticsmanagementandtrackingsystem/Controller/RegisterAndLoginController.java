f2509282d1e71b2b0ac0dbe1cd4e6d778cb94c16
adithyapurama <dummy@gmail.com>
2026-03-28 04:58:37 +0000

    cargo and status responses

--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/RegisterAndLoginController.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/RegisterAndLoginController.java
@@ -1,6 +1,5 @@
 package com.edutech.logisticsmanagementandtrackingsystem.Controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,15 +25,113 @@ import com.edutech.logisticsmanagementandtrackingsystem.service.CustomerService;
 import com.edutech.logisticsmanagementandtrackingsystem.service.DriverService;
 import com.edutech.logisticsmanagementandtrackingsystem.service.UserService;
 
+@RestController
+@RequestMapping
 public class RegisterAndLoginController {
-
         // register user in user repository by user service
         // after register in user repository then based on provided user role, register user in business, customer or driver repository
         // return with registered user 200 OK
         // implement login logic here
         // return valid jwt token in loginResponse
         // return 401 unauthorized if login failed
-    
+    @Autowired
+    private UserService userService;
 
+    @Autowired
+    private BusinessService businessService;
 
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private DriverService driverService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    /**
+     * Register user based on role
+     */
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+
+        // Save user in User table
+        User savedUser = userService.registerUser(user);
+
+        // Role-based registration
+        switch (savedUser.getRole().toUpperCase()) {
+
+            case "BUSINESS":
+                Business business = new Business();
+                business.setUser(savedUser);
+                businessService.createBusiness(business);
+                break;
+
+            case "CUSTOMER":
+                Customer customer = new Customer();
+                customer.setUser(savedUser);
+                customerService.createCustomer(customer);
+                break;
+
+            case "DRIVER":
+                Driver driver = new Driver();
+                driver.setUser(savedUser);
+                driverService.createDriver(driver);
+                break;
+
+            default:
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "Invalid role specified"
+                );
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(savedUser);
+    }
+
+    /**
+     * Login user and generate JWT token
+     */
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),
+                            loginRequest.getPassword()
+                    )
+            );
+
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "Invalid username or password"
+            );
+        }
+
+        // Load authenticated user details
+        UserDetails userDetails = userService
+                .loadUserByUsername(loginRequest.getUsername());
+
+        // Generate JWT token
+        String token = jwtUtil.generateToken(userDetails);
+
+        // Fetch user entity for response data
+        User user = userService
+                .getUserByUsername(loginRequest.getUsername());
+
+        LoginResponse response = new LoginResponse();
+        response.setToken(token);
+        response.setUsername(user.getUsername());
+        response.setEmail(user.getEmail());
+        response.setRole(user.getRole());
+
+        return ResponseEntity.ok(response);
+    }
 }
